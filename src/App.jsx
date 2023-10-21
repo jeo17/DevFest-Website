81060e954237e8477e42adf0aa8c2708f3d0db4e
jeo <abdeldjalildh990@gmail.com>
2023-10-21 18:34:39 +0200

    create project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { useState } from 'react'
+import   NavBar from './components/NavBar';
+import   Hero from './components/Hero';
+import   About from './components/About';
+import   Speakers from './components/Speakers';
+import   Topic from './components/Topic';
+import   Footer from './components/Footer';
+//import { useState } from 'react';
+
 
 
 import './App.css'
@@ -7,7 +14,12 @@ function App() {
 
   return (
     <>
-    
+    <NavBar />
+    <Hero />
+    <About />
+    <Speakers />
+    <Topic />
+    <Footer />
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <>
+        <h1 style={{color:"red"}}>navbar</h1>
+            
+        </>
+    );
+}
+
+export default NavBar;
